72c09311ec16e2649d4384d5ec9c327a18898623
zr <17628113560@16.com>
2020-12-26 10:54:05 +0800

    前置推送重贴标签

--- graph/relabel_to_front.cpp
+++ graph/relabel_to_front.cpp
@@ -3,9 +3,17 @@
 //
 
 /*
- *
+ * 最大流：前置重贴标签算法
+ *  许可边/许可网络
+ *  邻接链表：u.current/u.next-neighbor/u.N.head
  */
 
+//
+
+template<typename T>
+void discharge(Graph<T> *graph, Graph<T> *rng, size_t u) {
+    while
+}
 
 template<typename T>
 std::vector<typename GraphAlgorithm::Edge> GraphAlgorithm::relabelToFront(Graph<T> *graph, size_t s, size_t t) {
